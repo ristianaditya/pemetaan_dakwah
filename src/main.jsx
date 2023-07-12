7bde64d9219712e2b94debfc901c316f156e45e9
MNAlfiansyah <MNAlfiansyah33@gmail.com>
2023-07-12 22:50:18 +0700

    template-bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/scss/bootstrap.scss'
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/animate.min.css";
+import "./assets/css/demo.css";
+
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-draw/dist/leaflet.draw.css';
 import 'leaflet-draw';
 import './assets/style/maps.scss';
 import 'typeface-roboto';
+import { BrowserRouter, Route, Switch, Redirect, Link, Router} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import AdminLayout from "./layouts/Admin.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/maps" ><App /></Route>
+      <Route path="/maps/admin" render={(props) => <AdminLayout {...props} />} />
+      {/* <Route exact path="/">
+         <Redirect to="/dashboard" /> : <PublicHomePage />
+      </Route> */}
+      {/* <Redirect from="/maps/admin" to="maps/admin/dashboard" /> */}
+    </Switch>
+  </BrowserRouter>
+);
+
